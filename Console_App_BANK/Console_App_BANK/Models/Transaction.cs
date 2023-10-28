cad2003fab88852a2fb4925364333405db9b72da
Ayshen Salahova <TU81UEBFJ@code.edu.az>
2023-10-28 19:44:24 +0400

    created Transaction class

---
+++ Console_App_BANK/Console_App_BANK/Models/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_BANK.Models
+{
+    internal class Transaction
+    {
+        static int Count;
+        public int TransactionId { get;}
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public bool TransactionType { get; set; }  //true - DEPOSIT, false - WITHDRAWAL
+
+        public Transaction(decimal amount, bool transactiontype)
+        {
+            Count++;
+            TransactionId = Count;
+            Amount = amount;
+            TransactionDate=DateTime.Now;
+            TransactionType = transactiontype;
+        }
+    }
+}
