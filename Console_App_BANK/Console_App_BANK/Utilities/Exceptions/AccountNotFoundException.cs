b68c3eae4d75fe05f21527ec73ae841cfa1381cd
Ayshen Salahova <TU81UEBFJ@code.edu.az>
2023-10-28 19:26:36 +0400

    created AccountNotFoundException exception

---
+++ Console_App_BANK/Console_App_BANK/Utilities/Exceptions/AccountNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_BANK.Utilities.Exceptions
+{
+    internal class AccountNotFoundException:Exception
+    {
+        public AccountNotFoundException(string message):base(message) { }
+    }
+}
