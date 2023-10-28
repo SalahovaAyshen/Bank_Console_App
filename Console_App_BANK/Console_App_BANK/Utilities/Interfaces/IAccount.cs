91896280445df5b47ccf75616ce7156b5c9c520b
Ayshen Salahova <TU81UEBFJ@code.edu.az>
2023-10-28 19:33:22 +0400

    created Interface

---
+++ Console_App_BANK/Console_App_BANK/Utilities/Interfaces/IAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_BANK.Utilities.Interfaces
+{
+    internal interface IAccount
+    {
+       int AccountId { get; set; }
+       decimal Balance { get; set; }
+
+        public void Deposit(double amount);
+        public void Withdraw(double amount);
+
+    }
+}
